1695cc2b874512aca66ca74b5aea62976482400f
Aristotelis Alatzas <aalatzas@2-E-5.42heilbronn.de>
2024-06-17 01:27:36 +0200

    no more data race , small issue still existing one random philo dont eat but the forks ist free??

--- mandatory/handel_fork.c
+++ mandatory/handel_fork.c
@@ -6,7 +6,7 @@
 /*   By: aalatzas <aalatzas@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/08 23:02:57 by aalatzas          #+#    #+#             */
-/*   Updated: 2024/06/16 20:51:13 by aalatzas         ###   ########.fr       */
+/*   Updated: 2024/06/17 01:06:01 by aalatzas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,49 +56,3 @@ void	try_take_fork(t_philo *ph)
 	}
 	pthread_mutex_unlock(&ph->env->lock_fork);
 }
-
-// void give_fork_back(t_philo *ph)
-// {
-//     pthread_mutex_lock(&ph->env->free_fork);
-//     if (ph->index == ph->env->philo_nbr)
-//     {
-//         pthread_mutex_unlock(&ph->env->l_fork[ph->index]);
-//         pthread_mutex_unlock(&ph->env->l_fork[1]);
-//     }
-//     else
-//     {
-//         pthread_mutex_unlock(&ph->env->l_fork[ph->index]);
-//         pthread_mutex_unlock(&ph->env->l_fork[ph->index + 1]);
-//     }
-//     pthread_mutex_unlock(&ph->env->free_fork);
-// }
-
-// void try_take_fork(t_philo *ph)
-// {
-//     if (death_check(ph->env) != true)
-//     {
-//         if (ph->env->philo_nbr == 1)
-//         {
-//             pthread_mutex_lock(&ph->env->l_fork[ph->index]);
-//             print_msg(ph->env, ph->index, "has taken a fork", E_TAKE_FORK);
-//             ft_sleep(ph->time_to_die * 1200);
-//         }
-//         else
-//         {
-//             if (ph->index == ph->env->philo_nbr)
-//             {
-//                 pthread_mutex_lock(&ph->env->l_fork[1]);
-//                 print_msg(ph->env, ph->index, "has taken a fork", E_TAKE_FORK);
-//                 pthread_mutex_lock(&ph->env->l_fork[ph->index]);
-//                 print_msg(ph->env, ph->index, "has taken a fork", E_TAKE_FORK);
-//             }
-//             else
-//             {
-//                 pthread_mutex_lock(&ph->env->l_fork[ph->index]);
-//                 print_msg(ph->env, ph->index, "has taken a fork", E_TAKE_FORK);
-//                 pthread_mutex_lock(&ph->env->l_fork[ph->index + 1]);
-//                 print_msg(ph->env, ph->index, "has taken a fork", E_TAKE_FORK);
-//             }
-//         }
-//     }
-// }
--- mandatory/philo_tasks.c
+++ mandatory/philo_tasks.c
@@ -6,7 +6,7 @@
 /*   By: aalatzas <aalatzas@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 02:04:21 by aalatzas          #+#    #+#             */
-/*   Updated: 2024/06/16 21:50:12 by aalatzas         ###   ########.fr       */
+/*   Updated: 2024/06/17 01:23:43 by aalatzas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,39 @@ unsigned long long print_msg(t_env *env, int index, char *msg,t_state state)
 	death = env->death;
 	pthread_mutex_unlock(&env->death_lock);
 	pthread_mutex_lock(&env->print_lock);
-	if (death == false)
+	if (death_check(env) != true)
 	{
 		time = get_time();
-		if (state == E_TAKE_FORK && death_check(env) != true)
+		if (state == E_TAKE_FORK)
 			printf(Y"%llu %d %s%s\n"R, time - env->s_t, index, msg, TAKE_FORK);
-		else if (state == E_EAT && death_check(env) != true)
+		else if (state == E_EAT)
 			printf(GREEN"%llu %d %s%s\n"R, time - env->s_t, index, msg, EAT);
-		else if (state == E_SLEEP && death_check(env) != true)
+		else if (state == E_SLEEP)
 			printf(MAGENTA"%llu %d %s%s\n"R, time - env->s_t, index, msg, SLEEP);
-		else if (state == E_THINKING && death_check(env) != true)
+		else if (state == E_THINKING)
 			printf(BLUE"%llu %d %s%s\n"R, time - env->s_t, index, msg, THINKING);
-		else if (state == E_DIE)
-			printf(RED"%llu %d %s%s\n"R, time - env->s_t, index, msg, DIE);
 	}
+	else if (state == E_DIE && death == true)
+		printf(RED"%llu %d %s%s\n"R, get_time() - env->s_t, index, msg, DIE);
 	pthread_mutex_unlock(&env->print_lock);
 	return (time);
+}
+
+int check_meals(t_philo *ph)
+{
+	int result;
+	pthread_mutex_lock(&ph->lock_philo_meals);
+	if (ph->env->min_meals != -1 && ph->env->philo_nbr > 1)
+	{
+		if (ph->philo_meals >= ph->env->min_meals)
+			result = 1;
+		else
+			result = 2;
+	}
+	else
+		result = 0;
+	pthread_mutex_unlock(&ph->lock_philo_meals);
+	return (result);
 }
 
 void	schedule_action2(t_philo *ph);
@@ -52,16 +69,6 @@ int death_check(t_env *env)
 	}
 	pthread_mutex_unlock(&env->death_lock);
 	return (0);
-}
-
-unsigned long long get_last_eat_time(t_philo *ph)
-{
-	unsigned long long last_time;
-
-	pthread_mutex_lock(&ph->lock_last_time_eat);
-	last_time = ph->last_eat_time;
-	pthread_mutex_unlock(&ph->lock_last_time_eat);
-	return (last_time);
 }
 
 void one_philo_die(t_env *env, int i)
@@ -82,31 +89,38 @@ void	*check_philo_death(void *arg)
 
 	env = (t_env *)arg;
 	i = 1;
-	pthread_mutex_lock(&env->ph[i].lock_philo_meals);
-	while (death_check(env) != true && env->min_meals < env->ph[i].philo_meals)
+	while (death_check(env) != true)
 	{
-		pthread_mutex_unlock(&env->ph[i].lock_philo_meals);
-		if (i > env->philo_nbr)
-			i = 1;
-		last_time = get_last_eat_time(&env->ph[i]);
+		if (check_meals(&env->ph[i]) == 1)
+			break ;
+		pthread_mutex_lock(&env->ph[i].lock_last_time_eat);
+		// last_time = get_last_eat_time(&env->ph[i]);
+		last_time = env->ph[i].last_eat_time;
 		curr_time = get_time();
 		if (env->philo_nbr == 1)
 			return (one_philo_die(env, i), NULL);
-		if (curr_time - last_time > (unsigned long long)env->ph[i].time_to_die)
+		// printf("last_time: %llu\n", last_time);
+		// printf("curr_time: %llu\n", curr_time);
+		// printf("curr_time - last_time: %llu\n", curr_time - last_time);
+		if (curr_time - last_time >= (unsigned long long)env->ph[i].time_to_die)
 		{
-			print_msg(env, env->ph[i].index,"died", E_DIE);
 			pthread_mutex_lock(&env->death_lock);
 			env->death = true;
 			pthread_mutex_unlock(&env->death_lock);
+			print_msg(env, env->ph[i].index, "died", E_DIE);
+			pthread_mutex_unlock(&env->ph[i].lock_last_time_eat);
 			return (0);
 		}
+		pthread_mutex_unlock(&env->ph[i].lock_last_time_eat);
 		i++;
-		usleep(50);
-		pthread_mutex_lock(&env->ph[i].lock_philo_meals);
+		if (i > env->philo_nbr)
+			i = 1;
+		usleep(100);
 	}
-	pthread_mutex_unlock(&env->ph[i].lock_philo_meals);
 	return (NULL);
 }
+
+
 
 void	sleep_or_think(t_philo *ph)
 {
@@ -120,35 +134,43 @@ void	sleep_or_think(t_philo *ph)
 void	*schedule_action(void *arg)
 {
 	t_philo	*ph;
-	unsigned long long eat_time;
+	// unsigned long long eat_time;
 
 	ph = (t_philo *)arg;
+	pthread_mutex_lock(&ph->env->start_sim_lock);
+	while (ph->env->start_sim == false)
+	{
+		if (ph->env->start_sim == true)
+			break ;
+		pthread_mutex_unlock(&ph->env->start_sim_lock);
+		pthread_mutex_lock(&ph->lock_last_time_eat);
+		ph->last_eat_time = get_time();
+		pthread_mutex_unlock(&ph->lock_last_time_eat);
+		usleep(50);
+		pthread_mutex_lock(&ph->env->start_sim_lock);
+	}
+	pthread_mutex_unlock(&ph->env->start_sim_lock);
 	if (ph->index % 2 == 0)
-		ft_sleep(ph->time_to_eat / 2);
+		ft_sleep(ph->time_to_eat / 10);
 	while (death_check(ph->env) != true)
 	{
 		try_take_fork(ph);
-		if (death_check(ph->env) == true)
-			break ;
-		eat_time = print_msg(ph->env, ph->index,"is eating", E_EAT);
 		pthread_mutex_lock(&ph->lock_last_time_eat);
-		ph->last_eat_time = eat_time;
+		ph->last_eat_time = print_msg(ph->env, ph->index, "is eating", E_EAT);
 		pthread_mutex_unlock(&ph->lock_last_time_eat);
 		ft_sleep(ph->time_to_eat);
 		give_fork_back(ph);
-		if (ph->env->min_meals != - 1 && ph->env->philo_nbr > 1)
+		if (ph->env->min_meals != -1 && ph->env->philo_nbr > 1)
 		{
-			// printf("philo_meals: %d ->%d von %d\n", ph->index, ph->philo_meals, ph->env->min_meals);
-			if (ph->philo_meals >= ph->env->min_meals)
-				break ;
 			pthread_mutex_lock(&ph->lock_philo_meals);
 			ph->philo_meals++;
 			pthread_mutex_unlock(&ph->lock_philo_meals);
-			give_fork_back(ph);
-			sleep_or_think(ph);
+			if (ph->philo_meals >= ph->env->min_meals)
+				break ;
 		}
-		else
-			sleep_or_think(ph);
+		sleep_or_think(ph);
+		// if (death_check(ph->env) == true)
+		// 	break ;
 	}
 	return (NULL);
 }
